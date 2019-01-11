212eff55e132b9efeb76014b0305abefa394487a
Oleksii <qacrispus@gmail.com>
2019-01-11 15:12:24 +0200

    Decrease timeout (#4)
    * decrease waitFor timeout to 4 sec
    
    * 0.2.2

--- src/waiters/wait-conditions.ts
+++ src/waiters/wait-conditions.ts
@@ -8,28 +8,32 @@ export class WaitConditions {
   public static async clickable(element: ElementFinder): Promise<void> {
     await waitFor(
       EC.elementToBeClickable(element) as () => Promise<boolean>,
-      `${element.locator()} was expected to be clickable`
+      `${element.locator()} was expected to be clickable`,
+      4 * 1000
     );
   }
 
   public static async visible(element: ElementFinder): Promise<void> {
     await waitFor(
       EC.visibilityOf(element) as () => Promise<boolean>,
-      `${element.locator()} was expected to be visible`
+      `${element.locator()} was expected to be visible`,
+      4 * 1000
     );
   }
 
   public static async invisible(element: ElementFinder): Promise<void> {
     await waitFor(
       EC.invisibilityOf(element) as () => Promise<boolean>,
-      `${element.locator()} was expected to be invisible`
+      `${element.locator()} was expected to be invisible`,
+      4 * 1000
     );
   }
 
   public static async arrayIsNotEmpty(array: ArrayFragment<Fragment>): Promise<void> {
     await waitFor(
       Predicates.isArrayNotEmpty(array),
-      'Array of fragments expected to be not empty'
+      'Array of fragments expected to be not empty',
+      4 * 1000
     );
   }
 }
